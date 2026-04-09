6461d40ad8998b4805fc22dc3e19fa52298c1a73
mimbout7 <mimbout7@gmail.com>
2026-04-09 10:37:05 +0300

    add header layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
